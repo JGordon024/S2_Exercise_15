9f7abaa9cd47c613985957d82a4689125f5eee05
Unknown <jgordon024@west-mec.org>
2019-04-12 18:04:40 -0700

    Finished Order, started Payment
    We finished the order file. We made it so that you could switch which product you were buying and how many, as well as the protection plan, and that would all affect how much the price was.

--- co_order.js
+++ co_order.js
@@ -34,6 +34,15 @@ window.addEventListener("load", function () {
       orderForm.elements.model.focus();
       //Calculate the cost of the order
       calcOrder();
+
+      //event handlers for the web form"
+      orderForm.elements.model.onchange = calcOrder;
+      orderForm.elements.qty.onchange = calcOrder;
+
+      var planOptions = document.querySelectorAll('input[name="protection"]');
+      for (var i = 0; i < planOptions.length; i++) {
+            planOptions[i].onclick = calcOrder;
+      }
 });
 
 function calcOrder() {
@@ -47,21 +56,38 @@ function calcOrder() {
 
       //Initial cost = model times quantity 
       var initialCost = mCost * quantity;
-      orderForm.elements.initialCost.value = initialCost;
+      orderForm.elements.initialCost.value = formatUSCurrency(initialCost);
 
       //retrieve the cost of the user's protection plan
-      var pCost = document.querySelector('input[name="protection":checked').value * quantity;
-      orderForm.elements.protectionCost.value = pCost;
+      var pCost = document.querySelector('input[name="protection"]:checked').value * quantity;
+      orderForm.elements.protectionCost.value = formatNumber(pCost, 2);
 
       //calculate the order subtotal
-      orderForm.elements.subtotal.value = initialCost + pCost;
+      orderForm.elements.subtotal.value = formatNumber(initialCost + pCost, 2);
 
       //Caluclate the sales tax
       var salesTax = 0.05 * (initialCost + pCost);
-      orderForm.elements.salesTax.value = salesTax;
+      orderForm.elements.salesTax.value = formatNumber(salesTax, 2);
 
       //Calculate the cost of the total order
       var totalCost = initialCost + pCost + salesTax;
-      orderForm.elements.totalCost.value = totalCost;
+      orderForm.elements.totalCost.value = formatUSCurrency(totalCost);
 
+      //Store the order details
+      orderForm.elements.modelName.value = orderForm.elements.model.options[mIndex].text;
+      orderForm.elements.protectionName.value = document.querySelector('input[name="protection"]:checked').nextSibiling.nodeValue;
+}
+
+function formatNumber(val, decimals) {
+      return val.toLocaleString(undefined, {
+            minimumFractionDegits: decimals,
+            maximumFractionDigits: decimals
+      });
+}
+
+function formatUSCurrency(val) {
+      return val.toLocaleString('en-US', {
+            style: "currency",
+            currency: "USD"
+      });
 }
